d9f88c45742003c9b35cf69b50df47307cdcfc6d
Ahtasham Abbas <ch.ahtasham123@gmail.com>
2023-06-23 21:21:29 +0500

    next-auth Provider added on UI side

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 import '@styles/globals.css'
 
 export const metadata = {
@@ -9,13 +10,15 @@ const RootLayout = ({children}) => {
     return (
         <html lang="en">
         <body>
-        <div className="main">
-            <div className="gradient"></div>
-        </div>
-        <main className={'app'}>
-            <Nav/>
-            {children}
-        </main>
+        <Provider>
+            <div className="main">
+                <div className="gradient"></div>
+            </div>
+            <main className={'app'}>
+                <Nav/>
+                {children}
+            </main>
+        </Provider>
         </body>
         </html>
     )
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+'use client';
 
-const Provider = () => {
-    return (
-        <div>
-            Provider
-        </div>
-    );
+import {SessionProvider} from 'next-auth/react'
+
+const Provider = ({children, session}) => {
+    console.log(session)
+    return <SessionProvider session={session}>
+        {children}
+    </SessionProvider>;
 };
 
 export default Provider;
