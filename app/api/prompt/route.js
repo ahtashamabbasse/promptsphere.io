a6b5ee4eb77fb007a592284f4efa90a26905413d
Arslan Abbas <arslanarainsdk554@gmail.com>
2023-07-24 23:22:58 +0500

    implemented different types of searches

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ export const GET = async (req) => {
    try {
       await connectToDB();
       const allPrompts = await Prompt.find().populate('creator');
-      return new Response(JSON.stringify(allPrompts), { status: 201 });
+      return new Response(JSON.stringify(allPrompts), { status: 200 });
    } catch (err) {
       return new Response('Failed to fetch', { status: 500 });
    }
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,49 +3,87 @@ import React, { useEffect, useState } from 'react';
 import PromptCard from '@components/PromptCard';
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  return (
-    <div className={'prompt_layout mt-16'}>
-      {data.map((post) => {
-        return (
-          <PromptCard
-            post={post}
-            key={post._id}
-            handleTagClick={handleTagClick}
-          />
-        );
-      })}
-    </div>
-  );
+   return (
+      <div className={'prompt_layout mt-16'}>
+         {data.map((post) => {
+            return (
+               <PromptCard
+                  post={post}
+                  key={post._id}
+                  handleTagClick={handleTagClick}
+               />
+            );
+         })}
+      </div>
+   );
 };
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState('');
-  const [posts, setPosts] = useState([]);
+   const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-      setPosts(data);
-    })();
-  }, []);
+   // Search states
+   const [searchText, setSearchText] = useState('');
+   const [searchTimeout, setSearchTimeout] = useState(0);
+   const [searchedResults, setSearchedResults] = useState([]);
 
-  const onSearchChangeHandler = () => {};
+   useEffect(() => {
+      (async () => {
+         const response = await fetch('/api/prompt');
+         const data = await response.json();
+         setPosts(data);
+      })();
+   }, []);
 
-  return (
-    <section className={'feed'}>
-      <form className={'flex-center relative w-full'}>
-        <input
-          type={'text'}
-          value={searchText}
-          className={'search_input peer'}
-          placeholder={'Search for a tag or a username'}
-          onChange={onSearchChangeHandler}
-        />
-      </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
-    </section>
-  );
+   const filterPrompt = (searchText) => {
+      const regex = new RegExp(searchText, 'i');
+      return posts.filter(
+         (item) =>
+            regex.test(item.creator.username) ||
+            regex.test(item.tag) ||
+            regex.test(item.prompt)
+      );
+   };
+   const onSearchChangeHandler = (event) => {
+      clearTimeout(searchTimeout);
+      setSearchText(event.target.value);
+
+      setSearchTimeout(
+         setTimeout(() => {
+            const searchResults = filterPrompt(event.target.value);
+            setSearchedResults(searchResults);
+            console.log(searchResults);
+         }, 500)
+      );
+   };
+   const handleTagClick = (tagName) => {
+      setSearchText(tagName);
+
+      const searchResult = filterPrompt(tagName);
+      setSearchedResults(searchResult);
+   };
+
+   return (
+      <section className={'feed'}>
+         <form className={'flex-center relative w-full'}>
+            <input
+               type={'text'}
+               value={searchText}
+               className={'search_input peer'}
+               placeholder={'Search for a tag or a username'}
+               onChange={onSearchChangeHandler}
+            />
+         </form>
+
+         {searchText ? (
+            <PromptCardList
+               data={searchedResults}
+               handleTagClick={handleTagClick}
+            />
+         ) : (
+            <PromptCardList data={posts} handleTagClick={handleTagClick} />
+         )}
+      </section>
+   );
 };
 
 export default Feed;
